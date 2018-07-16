64d42125a4a54a73792b77ae73fb328b82a7dc48
wang <13269403044@163.com>
2018-07-16 14:00:54 +0800

    webpack-demo

---
+++ 3-5 提取公共代码/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: {
+        'pageA': './src/pageA',
+        'pageB': './src/pageB',
+        'vendor': ['lodash']
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].chunk.js'
+    },
+
+    // 提取公共代码
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: 'vendor',
+            minChunks: Infinity
+        })
+    ]
+}
